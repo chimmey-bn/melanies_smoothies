88382b4cfe6797804d8f13a2ab9d513d5484f746
chimmey-bn <veenathippeswamy@gmail.com>
2026-04-21 18:06:50 -0700

    Change st.data_frame to st.dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 # st.dataframe(data=my_dataframe, width = 'content')
 # st.stop()
 pd_df = my_dataframe.to_pandas()
-st.data_frame(pd_df)
+st.dataframe(pd_df)
 st.stop()
 ingredient_list = st.multiselect(
  'Choose up to 5 ingredients:'
